9b8ceed194fdc82ab6a54ea25756b71cb7a44d32
rajasekarcloud <rajasekar.unix@yahoo.com>
2024-07-25 20:40:01 -0700

    Done

--- Ex1.py
+++ Ex1.py
@@ -1,4 +1,5 @@
 a = [1,2,3,4,5,6,7,8,9];
 print(a[::2]);
-a[::2]=10,20,30,40,50,60;
+#a[::2]=10,20,30,40,50,60; # ValueError: attempt to assign sequence of size 6 to extended slice of size 5
 print(a);
+print(a[::3]);
